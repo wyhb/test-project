75191437fd271a3d1765ad6998df810337c9e731
Admin <linuxangel@126.com>
2023-02-19 11:41:46 +0800

    作手工git操作

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <hr />
+  <test-sea>
+    <div>1111111</div>
+    <h2>H22222222</h2>
+  </test-sea>
+  <hr />
+  <test-component />
+  <hr />
+  Vue3 Template
+  <H2>ejs动态渲染模板: test-project1.0.8</H2>
+  <hr />
+  <div>
+    <span><router-link to="/home">主  页 | </router-link></span>
+    <span><router-link to="/order">订 单 | </router-link></span>
+    <span><router-link to="/my">个人中心</router-link></span>
+  </div>
+  <hr />
+  <button @click="jump">jump</button>
+  <router-view />
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
+export default {
+  name: 'App',
+  setup() {
+    const router = useRouter();
+    return {
+      jump() {
+        router.push('/order');
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="home"><h2>Home 网站首页</h2></div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Home'
+}
+</script>
